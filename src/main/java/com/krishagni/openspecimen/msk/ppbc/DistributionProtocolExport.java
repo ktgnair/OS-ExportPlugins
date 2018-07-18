02c80d790307d5987dcaa42473eb71fda28ce719
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2018-07-18 15:41:53 +0530

    SI: Added: Exported the custom fields for DistributionProtocol to output .CSV sheet.

--- src/main/java/com/krishagni/openspecimen/msk/ppbc/DistributionProtocolExport.java
+++ src/main/java/com/krishagni/openspecimen/msk/ppbc/DistributionProtocolExport.java
@@ -3,8 +3,10 @@ package com.krishagni.openspecimen.msk.ppbc;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
@@ -20,6 +22,7 @@ import com.krishagni.catissueplus.core.biospecimen.repository.DaoFactory;
 import com.krishagni.catissueplus.core.common.PlusTransactional;
 import com.krishagni.catissueplus.core.common.util.ConfigUtil;
 import com.krishagni.catissueplus.core.common.util.CsvFileWriter;
+import com.krishagni.catissueplus.core.de.domain.DeObject.Attr;
 
 @Configurable
 public class DistributionProtocolExport implements ScheduledTask {
@@ -34,7 +37,7 @@ public class DistributionProtocolExport implements ScheduledTask {
 	
 	private void exportDP() {
 		CsvFileWriter csvFileWriter = null;
-		DpRequirementExport dprExport = new DpRequirementExport();
+		DpRequirementExport dpRExport = new DpRequirementExport();
 		
 		try {
 			csvFileWriter = getCSVWriter();
@@ -44,14 +47,14 @@ public class DistributionProtocolExport implements ScheduledTask {
 			int startAt = 0, maxRecs = 100;
 		    
 			while (!endOfDPs) {
-      			int exportedRecsCount = exportDPs(csvFileWriter, startAt, maxRecs, dprExport);
+      			int exportedRecsCount = exportDPs(csvFileWriter, startAt, maxRecs, dpRExport);
       			startAt += exportedRecsCount;
       			endOfDPs = (exportedRecsCount < maxRecs);
     		}
   		} catch (Exception e) {
   			logger.error("Error while running distribution protocol export job", e);
 		} finally {
-			dprExport.closeWriter();
+			dpRExport.closeWriter();
 			IOUtils.closeQuietly(csvFileWriter);
 		}
 	}
@@ -86,22 +89,52 @@ public class DistributionProtocolExport implements ScheduledTask {
 	}
 
 	private String[] getHeader() {
-		return new String[] {
-				"TBR_REQUEST_TITLE", 
-				"TBR_SOURCE_REQUEST", 
-				"TBR_INSTITUTE_DESC", 
-				"TBR_DEPT_DESC", 
-				"TBR_REQUESTER_DESC"
+		return new String[]{
+			"TBR_REQUEST_TITLE",			// Title
+			"TBR_SOURCE_REQUEST",			// Shorttitle
+			"TBR_INSTITUTE_DESC",			// ReceivingInstitute
+			"TBR_DEPT_DESC",			// Department
+			"TBR_REQUESTER_DESC",			// PI#Email
+			"TBR_REQUEST_DT",			// DPCustomFields#Request
+			"TBR_ILAB_NO",				// DateDPCustomFields#ILABNo
+			"TBR_FINALIZE_FLAG",			// DPCustomFields#FinalizeFlag
+			"TBR_COST_CENTER",			// DPCustomFields#CostCenter
+			"TBR_FUND_ID",				// DPCustomFields#FundID
+			"TBR_MTA_FLAG",				// DPCustomFields#MTAFlag
+			"TBR_DISTRIBUTION_OPTION_DESC",		// DPCustomFields#DistributionOptionDesc
+			"TBR_HBC_ID",				// DPCustomFields#HBCID
+			"TBR_HBC_COMMITTEE_APPROVAL_DT",	// DPCustomFields#HBCCommitteeApprovalDate
+			"TBR_MIN_UNIT",				// DPCustomFields#MinimumUnit
+			"TBR_MTA_APPROVAL_DT",			// DPCustomFields#MTAApprovalDate
+			"TBR_PICKUP_ARRANGEMENT_DESC",		// DPCustomFields#PickupArrangementDescription
+			"TBR_PROSPECT_FLAG",			// DPCustomFields#ProspectFlag
+			"TBR_TYPE_DESC",			// DPCustomFields#TypeDescription
+			"TBR_RESTROSPECT_FLAG",			// DPCustomFields#RestrospectFlag
+			"TBR_SPECIMEN_COLLECTION_METHOD",	// DPCustomFields#SpecimenCollectionMethod
+			"TBR_COMMENTS",				// DPCustomFields#Comments
+			"TBR_CONTACT_NAME",			// DPCustomFields#ContactName
+			"TBR_SPECIMEN_USAGE_DESC"		// DPCustomFields#SpecimenUsage
 		};
 	}
 
 	private String[] getRow(DistributionProtocol dp) {
-		return new String[] { 
-				dp.getTitle(), 
-				dp.getShortTitle(), 
-				dp.getInstitute().getName(), 
-				dp.getDefReceivingSite().getName(), 
-				dp.getPrincipalInvestigator().getFirstName() + " " + dp.getPrincipalInvestigator().getLastName()
-		};
+		List<String> row = new ArrayList<>();
+
+		row.add(dp.getTitle());
+		row.add(dp.getShortTitle());
+		row.add(dp.getInstitute().getName());
+		row.add(dp.getDefReceivingSite().getName());
+		row.add(dp.getPrincipalInvestigator().getFirstName() + " " + dp.getPrincipalInvestigator().getLastName());
+		row.addAll(getCustomFieldValues(dp));
+		
+		return row.toArray(new String[row.size()]);
 	}
-}
+	
+	private List<String> getCustomFieldValues(DistributionProtocol dp) {
+		return dp.getExtension()
+			.getAttrs().stream()
+			.map(Attr::getDisplayValue)
+			.collect(Collectors.toList());
+	}
+}	
+
