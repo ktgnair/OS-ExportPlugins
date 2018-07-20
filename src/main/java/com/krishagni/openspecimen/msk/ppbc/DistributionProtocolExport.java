2967614e131ac37d8f7d9249700c1bc01c3c09ef
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2018-07-20 10:58:21 +0530

    SI: Added: 1. Added 'Distribution Order' section in the code that hosts the method neccessary for exporting Distribution Orders. 2. Changed the method name getDpCustomFieldValues() -> getCustomFieldValues() and made it generic.
    Note:
    1. While traversing the Distribution Protocols we are passing dp.getDistributionOrders(), for each DP, to exportDOs(csvWriter, Collection<DistributionOrder> distributionOrders) method.

--- src/main/java/com/krishagni/openspecimen/msk/ppbc/DistributionProtocolExport.java
+++ src/main/java/com/krishagni/openspecimen/msk/ppbc/DistributionProtocolExport.java
@@ -15,11 +15,14 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 
+import com.krishagni.catissueplus.core.administrative.domain.DistributionOrder;
+import com.krishagni.catissueplus.core.administrative.domain.DistributionOrderItem;
 import com.krishagni.catissueplus.core.administrative.domain.DistributionProtocol;
 import com.krishagni.catissueplus.core.administrative.domain.DpRequirement;
 import com.krishagni.catissueplus.core.administrative.domain.ScheduledJobRun;
 import com.krishagni.catissueplus.core.administrative.repository.DpListCriteria;
 import com.krishagni.catissueplus.core.administrative.services.ScheduledTask;
+import com.krishagni.catissueplus.core.biospecimen.domain.BaseExtensionEntity;
 import com.krishagni.catissueplus.core.biospecimen.repository.DaoFactory;
 import com.krishagni.catissueplus.core.common.PlusTransactional;
 import com.krishagni.catissueplus.core.common.util.ConfigUtil;
@@ -38,19 +41,22 @@ public class DistributionProtocolExport implements ScheduledTask {
 	}
 	
 	private void export() {
-		CsvFileWriter dpFileWriter = null, dPRFileWriter = null;
+		CsvFileWriter dpFileWriter = null, dPRFileWriter = null, doFileWriter = null;
 		
 		try {
 			dpFileWriter = getDpCSVWriter();
 			dPRFileWriter = getDpRCSVWriter();
+			doFileWriter = getDoCSVWriter();
+			
 			dpFileWriter.writeNext(getDpHeader());
 			dPRFileWriter.writeNext(getDpRHeader());
+			doFileWriter.writeNext(getDoHeader());
 			
 			boolean endOfDPs = false;
 			int startAt = 0, maxRecs = 100;
 		    
 			while (!endOfDPs) {
-      			int exportedRecsCount = exportDpData(dpFileWriter, dPRFileWriter, startAt, maxRecs);
+      			int exportedRecsCount = exportDpData(dpFileWriter, dPRFileWriter, doFileWriter, startAt, maxRecs);
       			startAt += exportedRecsCount;
       			endOfDPs = (exportedRecsCount < maxRecs);
     		}
@@ -59,23 +65,33 @@ public class DistributionProtocolExport implements ScheduledTask {
 		} finally {
 			IOUtils.closeQuietly(dPRFileWriter);
 			IOUtils.closeQuietly(dpFileWriter);
+			IOUtils.closeQuietly(doFileWriter);
 		}
 	}
-	
+
 	@PlusTransactional
-	private int exportDpData(CsvFileWriter dpFileWriter, CsvFileWriter dPRFileWriter, int startAt, int maxRecs) throws IOException {
+	private int exportDpData(CsvFileWriter dpFileWriter, CsvFileWriter dPRFileWriter, CsvFileWriter doFileWriter, int startAt, int maxRecs) throws IOException {
 		DpListCriteria listCrit = new DpListCriteria().startAt(startAt).maxResults(maxRecs);
 		List<DistributionProtocol> dPs = daoFactory.getDistributionProtocolDao().getDistributionProtocols(listCrit);
 		
 		exportDp(dpFileWriter, dPs);
 		dPs.forEach(dp -> exportDpr(dPRFileWriter, dp.getRequirements()));
+		dPs.forEach(dp -> exportDOs(doFileWriter, dp.getDistributionOrders()));
 		
 		dpFileWriter.flush();
 		dPRFileWriter.flush();
+		doFileWriter.flush();
 
 		return dPs.size();
 	}
 	
+	private List<String> getCustomFieldValues(BaseExtensionEntity obj) {
+		return obj.getExtension()
+			.getAttrs().stream()
+			.map(Attr::getDisplayValue)
+			.collect(Collectors.toList());
+	}
+
 	///////////////////////
 	//
 	// DP export
@@ -129,16 +145,9 @@ public class DistributionProtocolExport implements ScheduledTask {
 		row.add(dp.getInstitute().getName());
 		row.add(dp.getDefReceivingSite().getName());
 		row.add(dp.getPrincipalInvestigator().getFirstName() + " " + dp.getPrincipalInvestigator().getLastName());
-		row.addAll(getDpCustomFieldValues(dp));
+		row.addAll(getCustomFieldValues(dp));
 		
 		return row.toArray(new String[row.size()]);
-	}
-	
-	private List<String> getDpCustomFieldValues(DistributionProtocol dp) {
-		return dp.getExtension()
-			.getAttrs().stream()
-			.map(Attr::getDisplayValue)
-			.collect(Collectors.toList());
 	}
 	
 	///////////////////////
@@ -179,5 +188,61 @@ public class DistributionProtocolExport implements ScheduledTask {
 				"TBRD_BILLING_AMT",
 				"TBRD_SOURCE_REQUEST"
 		};
+	}
+	
+	///////////////////////
+	//
+	// Distribution Orders
+	//
+	///////////////////////
+	
+	private CsvFileWriter getDoCSVWriter() {
+		String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+		File outputFile = new File(ConfigUtil.getInstance().getDataDir(), "DistributionOrders_" + timestamp + ".csv");
+		return CsvFileWriter.createCsvFileWriter(outputFile);
+	}
+	
+	private String[] getDoHeader() {
+		return new String[] {
+				"Name",
+				"TBDS_DISTRIBUTION_DT",
+				"TBDS_SOURCE_REQUEST",
+				"TBDS_BILLING_AMT",
+				"Specimen Label",
+				"TBDS_BILLING_DT"
+		};
+	}
+
+	private void exportDOs(CsvFileWriter doFileWriter, Set<DistributionOrder> distributionOrders) {
+		if (!distributionOrders.isEmpty()) {
+			distributionOrders.forEach(distributionOrder -> writeDoToCsv(doFileWriter, distributionOrder.getOrderItems()));
+		}
+	}
+	
+	private void writeDoToCsv(CsvFileWriter doFileWriter, Set<DistributionOrderItem> orderItems) {
+		if (!orderItems.isEmpty()) {
+			orderItems.forEach(item -> doFileWriter.writeNext(getDoRow(item)));
+		}
+	}
+
+	private String[] getDoRow(DistributionOrderItem item) {
+		List<String> row = new ArrayList<String>();
+		
+		row.add(item.getOrder().getName());
+		row.add(item.getOrder().getExecutionDate().toString());
+		row.add(item.getOrder().getDistributionProtocol().getShortTitle());
+		row.add(getItemCost(item));
+		row.add(item.getSpecimen().getLabel());
+		row.addAll(getCustomFieldValues(item.getOrder()));
+		
+		return row.toArray(new String[row.size()]);
+	}
+
+	private String getItemCost(DistributionOrderItem item) {
+		if (item.getCost() != null) {
+			return item.getCost().toString();
+		} else {
+			return "";
+		}
 	}
 }	
